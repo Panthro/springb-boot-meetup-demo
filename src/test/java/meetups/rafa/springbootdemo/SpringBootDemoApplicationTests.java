5c69d9ed8a5f804d1b626a27df56e6db24353e18
rafael Roman <rafael.roman@gft.com>
2019-05-03 14:49:07 +0200

    add simple test

--- src/test/java/meetups/rafa/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/meetups/rafa/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,16 +1,53 @@
 package meetups.rafa.springbootdemo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import meetups.rafa.springbootdemo.domain.Hero;
+import meetups.rafa.springbootdemo.repository.HeroRepository;
+
+import static meetups.rafa.springbootdemo.domain.Hero.Universe.DC;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    HeroRepository heroRepository;
+
+
+    @Before
+    public void setUp() {
+        heroRepository.deleteAll();
     }
+
+    @After
+    public void tearDown() {
+        heroRepository.deleteAll();
+    }
+
+    private void addBatman() {
+        final Hero hero = new Hero();
+        hero.setName("Batman");
+        hero.setUniverse(DC);
+        heroRepository.save(hero);
+    }
+
+    @Test
+    public void createHero() {
+        final long initialCount = heroRepository.count();
+
+        addBatman();
+
+        assertThat(heroRepository.count()).isGreaterThan(initialCount);
+
+    }
+
 
 }
