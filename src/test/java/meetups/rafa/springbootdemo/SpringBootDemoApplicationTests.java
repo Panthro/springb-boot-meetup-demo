58bd89894cca611388abdfe3d25c02cda8f154d4
rafael Roman <rafael.roman@gft.com>
2019-05-03 14:50:52 +0200

    add web test

--- src/test/java/meetups/rafa/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/meetups/rafa/springbootdemo/SpringBootDemoApplicationTests.java
@@ -5,22 +5,31 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import meetups.rafa.springbootdemo.domain.Hero;
 import meetups.rafa.springbootdemo.repository.HeroRepository;
 
 import static meetups.rafa.springbootdemo.domain.Hero.Universe.DC;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringBootDemoApplicationTests {
 
     @Autowired
     HeroRepository heroRepository;
 
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Before
     public void setUp() {
@@ -46,6 +55,21 @@ public class SpringBootDemoApplicationTests {
         addBatman();
 
         assertThat(heroRepository.count()).isGreaterThan(initialCount);
+
+    }
+
+
+    @Test
+    public void webGetHeroes() throws Exception {
+
+        addBatman();
+
+        mockMvc
+                .perform(get("/heroes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content[0].name").value("Batman"));
+
 
     }
 
